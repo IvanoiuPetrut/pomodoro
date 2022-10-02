996316133b975801c71cd7f2d1c1f64605ea7627
Petrut <ivanoiu.petrut27@yahoo.com>
2022-10-02 15:20:33 +0300

    wip: tried rendering GLTF file

--- src/components/TreeProgress.vue
+++ src/components/TreeProgress.vue
@@ -1,10 +1,38 @@
 <template>
-  <img alt="Tree" src="../assets/img/tree.png" />
-  <div></div>
+  <img src="../assets/img/tree.png" alt="tree" />
+  <div>
+    <Renderer>
+      <Camera :position="{ z: 10 }" />
+      <Scene>
+        <GltfModel :src="getImgPath('mc')" @load="onReady" />
+      </Scene>
+    </Renderer>
+  </div>
 </template>
 
 <script lang="ts">
-export default {};
+import { Camera, Renderer, Scene, GltfModel } from "troisjs";
+export default {
+  components: { Camera, Renderer, Scene, GltfModel },
+
+  setup() {
+    const onReady = (model: unknown) => {
+      console.log(model);
+    };
+
+    const getImgPath = (pic: string) => {
+      // return require(`../assets/img/${pic}.gltf`);
+      return `../assets/img/${pic}.gltf`;
+      // return require(`!!raw-loader!../assets/img/${pic}.gltf`);
+      // return import(`../assets/img/${pic}.gltf`);
+    };
+
+    return {
+      onReady,
+      getImgPath,
+    };
+  },
+};
 </script>
 
 <style lang=""></style>
