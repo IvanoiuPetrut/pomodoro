467c84efbba46664bb318ccfcf20b69da7f38b22
Petrut <ivanoiu.petrut27@yahoo.com>
2022-10-02 14:34:39 +0300

    feat: add TroisJS

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <h1>Pomodoro</h1>
+  <TreeProgress></TreeProgress>
   <TimerProgress></TimerProgress>
 </template>
 
 <script lang="ts">
 import TimerProgress from "./components/TimerProgress.vue";
+import TreeProgress from "./components/TreeProgress.vue";
 
 export default {
   name: "App",
   components: {
     TimerProgress,
+    TreeProgress,
   },
 };
 </script>
---
+++ src/components/TreeProgress.vue
@@ -0,0 +1,10 @@
+<template>
+  <img alt="Tree" src="../assets/img/tree.png" />
+  <div></div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style lang=""></style>
